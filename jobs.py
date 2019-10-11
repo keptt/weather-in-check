bf1f48944218afe28adb7b3bf0a76572b12119e9
keptt <blackoutxray@gmail.com>
2019-10-11 20:36:50 +0300

    .

--- jobs.py
+++ jobs.py
@@ -7,7 +7,7 @@ import os
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri, sun', hour='20', minute='36', timezone=default.TIMEZONE_STR)
+@sched.scheduled_job('cron', day_of_week='mon-fri, sun', hour='18', minute='05', timezone=default.TIMEZONE_STR)
 def query_weather():
     sm.send_mail(login=default.SMTP_SEND_FROM, password=default.SMTP_PASSWORD, send_to=default.SMTP_SEND_TO, subject='xxx', message='yyyy')#main()
 #sm.send_mail(login=default.SMTP_SEND_FROM, password=default.SMTP_PASSWORD, send_to=default.SMTP_SEND_TO, subject='adsgtasg', message='1231432')
