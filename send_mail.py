7b472ff3064804253afc19166a3452f1567d0769
keptt <blackoutxray@gmail.com>
2019-10-11 20:21:10 +0300

    .

--- send_mail.py
+++ send_mail.py
@@ -5,5 +5,5 @@ def send_mail(login, password, send_to, subject, message, service='smtp.gmail.co
     smtp_obj.ehlo()
     smtp_obj.starttls()
     smtp_obj.login(login, password)
-    smtp_obj.sendmail(login, send_to, f'{subject}\n{message}')
+    smtp_obj.sendmail(login, send_to, f'Subject: {subject}\n{message}')
     smtp_obj.quit()
