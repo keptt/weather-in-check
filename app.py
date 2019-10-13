18062870b940c3a0c077e2fd46e738801f426261
keptt <blackoutxray@gmail.com>
2019-10-13 15:14:23 +0300

    simple fixes

--- app.py
+++ app.py
@@ -16,9 +16,6 @@ def main():
 
     fc = owm.three_hours_forecast_at_coords(default.COORD_X, default.COORD_Y)
     f = fc.get_forecast()                                                       #return list of weather objects that contain weather info mapped with the time of the day when such weathter is to be expected
-    print(f)
-
-    #print(type(tommorow_weather.get_reference_time('date')), tommorow_weather.get_status(), tommorow_weather.get_temperature(unit='celsius'))
 
     med: float = 0
     count: int = 0
@@ -31,9 +28,7 @@ def main():
     for weather in f:                                                                   
         date_of_forecast = weather.get_reference_time('date')                                                       # receive date from weather object
 
-        # print('tommorow:')
         if date_of_forecast.hour > 6 and date_of_forecast.hour < 21 and date_of_forecast.date() == tommorow_date:   # considering only temperature data from 6 in the morning til 21 o'clock
-            # print(weather.get_reference_time('iso'), weather.get_status(), weather.get_temperature(unit='celsius'))
         
             count += 1
             detailed_report += (' '.join([str(weather.get_reference_time('iso')), str(weather.get_status()), str(weather.get_temperature(unit='celsius')), '\n']))  # form report to later send via male.
@@ -52,11 +47,6 @@ def main():
     with open(default.PICKLE_FILE, 'bw') as pickle_handle: 
         tommorows_temperature = med/count
         pickle.dump(med/count, pickle_handle)
-    
-    print('todays_temprature: ', todays_temprature)
-    print('tommorows_temperature: ', tommorows_temperature)
-    print('abs(tommorows_temperature - todays_temprature): ', abs(tommorows_temperature - todays_temprature))
-    print('(todays_temprature/100)*15: ', (todays_temprature/100)*15)
 
     if abs(tommorows_temperature - todays_temprature) > (todays_temprature/100)*15:                                                     # difference in more than 15%//4 degrees
         subject = f'Subject: TEMP DIFF {todays_temprature} vs {tommorows_temperature}'
--- send_mail.py
+++ send_mail.py
@@ -1,14 +1,17 @@
-import smtplib
+import smtplib, ssl
 from email.mime.text import MIMEText
 
 def send_mail(login, password, send_to, subject, message, service='smtp.gmail.com', port=587):
-    prepared_message = MIMEText(message)
+    if isinstance(send_to, str):
+        send_to = [send_to]                                  # make a list out if send_to argument if it was passed as a string
+    prepared_message = MIMEText(message, 'plain')            # here second argument is optional
     prepared_message['From'] = login
     prepared_message['To'] = ', '.join(send_to)
     prepared_message['Subject'] = subject
     
-    smtp_obj = smtplib.SMTP(service, port)
-    smtp_obj.starttls()
-    smtp_obj.login(login, password)
-    smtp_obj.sendmail(login, send_to, prepared_message.as_string())
-    smtp_obj.quit()
+    context = ssl.create_default_context()
+    
+    with smtplib.SMTP(service, port) as smtp_obj:
+        smtp_obj.starttls(context=context)                  # encrypt your smtp connection
+        smtp_obj.login(login, password)
+        smtp_obj.sendmail(login, send_to, prepared_message.as_string())
